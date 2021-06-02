5e1ceb7b73eb572346ec50730d4044f71d2789e9
Alan Oliveira <alanjhone@gmail.com>
2021-06-01 21:04:04 -0300

    feat: adicionando micro serviço de catalago de video

---
+++ videos/src/test/java/br/com/codeflix/videos/VideosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codeflix.videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
